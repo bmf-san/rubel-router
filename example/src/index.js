1af0d59783694224d79423b2342cf5fd99896815
bmf <bmf.infomation@gmail.com>
2018-01-20 23:04:50 +0900

    [update]

--- example/src/index.js
+++ example/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
 import registerServiceWorker from './registerServiceWorker';
+import {Route} from 'rubel-router';
 
+console.log(Route);
 ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
