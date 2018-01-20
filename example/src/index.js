ccd867f7a68feb64e19152f054750bd89930d0a0
bmf <bmf.infomation@gmail.com>
2018-01-21 01:55:09 +0900

    [update]

--- example/src/index.js
+++ example/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
 import registerServiceWorker from './registerServiceWorker';
-import {Router} from 'rubel-router';
+import Router from 'rubel-router';
 
 console.log(Router);
 ReactDOM.render(<App/>, document.getElementById('root'));
